29146f3989e7f0af7dd2d4701c42a466fa86274f
Milo Stroik <mmstroik@gmail.com>
2026-03-17 17:44:07 -0400

    fix: update native binding version checks to 0.2.1 and adjust package dependencies

--- query-parser.wasi.cjs
+++ query-parser.wasi.cjs
@@ -39,7 +39,7 @@ if (__nodeFs.existsSync(__wasmDebugFilePath)) {
   __wasmFilePath = __wasmDebugFilePath
 } else if (!__nodeFs.existsSync(__wasmFilePath)) {
   try {
-    __wasmFilePath = __nodePath.resolve('@alidade/query-parser-wasm32-wasi')
+    __wasmFilePath = require.resolve('@alidade/query-parser-wasm32-wasi/query-parser.wasm32-wasi.wasm')
   } catch {
     throw new Error('Cannot find query-parser.wasm32-wasi.wasm file, and @alidade/query-parser-wasm32-wasi package is not installed.')
   }
